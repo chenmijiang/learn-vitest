28fcb16c4d8459c472668a4d520fa2023e39e7a9
chenmijiang <jack.chenyuana@gmail.com>
2026-05-27 09:03:33 +0800

    feat: add global setup and teardown functions for testing

--- vitest.config.ts
+++ vitest.config.ts
@@ -4,7 +4,7 @@ import { defineConfig } from "vitest/config";
 
 export default defineConfig({
   test: {
-    globalSetup: "./globalSetup.ts",
+    globalSetup: "./setup/globalSetup.ts",
     projects: [
       {
         test: {
